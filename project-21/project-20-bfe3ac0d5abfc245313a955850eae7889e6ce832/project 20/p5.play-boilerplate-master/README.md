9ecf05bf79d485d57b9118baea4975e358dba95d
harshvardhanPhalke07 <75940704+harshvardhanPhalke07@users.noreply.github.com>
2020-12-31 21:19:57 +0530

    Add files via upload

---
+++ project-21/project-20-bfe3ac0d5abfc245313a955850eae7889e6ce832/project 20/p5.play-boilerplate-master/README.md
@@ -0,0 +1,8 @@
+# p5.play-boilerplate
+Boiler plate for p5.play
+so, seeing the effectiveness of my previous car crash simulator,
+/*the company has funded me to create this simulator*/
+so, this one simulates bullets hitting a wall.
+each wall has different materials to be tested by hitting those bullets to /*each wall*/
+if a material recieves damage **more then 10**, it will be rejected.
+But if a material recieves damage **less than 10** it will be accepted and used in making the wall
---
+++ project-21/project-20-bfe3ac0d5abfc245313a955850eae7889e6ce832/project 20/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,37 @@
+var bullet,wall 
+var weight, speed
+var thickness
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+  speed= random(223,321)
+  weight=random(30,52)
+  thickness=random(22,83)
+  bullet=createSprite(50,200.50,50)
+  wall=createSprite(1200,200,thickness,height/2)
+  wall.shapeColor=(80,80,80)
+  bullet.velocityX=speed;
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+ 
+}
+
+function hasCollided(){
+  bulletRightEdge=bullet.x+bullet.width;
+  wallLeftEdge=wall.x
+  if (bulletRightEdge>=wallLeftEdge){
+    return true
+  }
+  return false 
+}
+if(hasCollided(bullet,wall)){
+  var damage =0.5*weight* speed* speed(thickness * thickness * thickness)
+  if(damage>10){
+    wall.shapeColor=color(255,0,0)
+  }
+  if (damage<10)
+  wall.shapeColor=color(0,255,0)
+}
